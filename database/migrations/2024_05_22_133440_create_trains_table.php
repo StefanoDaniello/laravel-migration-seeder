b05c9775765c98d601c220bdd7b5dcda87891556
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-05-22 15:54:50 +0200

    create database

---
+++ database/migrations/2024_05_22_133440_create_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda',255);
+            $table->string('stazione_di_partenza',255);
+            $table->time('stazione_di_arrivo');
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('Codice_Treno',255);
+            $table->float('Numero_carrozze');
+            $table->boolean('In_orario');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
